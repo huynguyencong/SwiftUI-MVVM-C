7c9c4cae6e6860fb4c024632c9826c02747894d5
Huy Nguyen <conghuy2012@gmail.com>
2021-05-17 11:47:03 +0700

    Add new project

---
+++ SwiftUI-MVVM-C/SwiftUI-MVVM-C/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM-C
+//
+//  Created by Nguyen Cong Huy on 5/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
