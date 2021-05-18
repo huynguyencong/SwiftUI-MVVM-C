e82a1eee09c8e0d6f021048103bbee0037e8a860
Huy Nguyen <conghuy2012@gmail.com>
2021-05-18 11:27:22 +0700

    Update Repo cell

--- SwiftUI-MVVM-C/SwiftUI-MVVM-C/Modules/RepoList/RepoList/RepoCell.swift
+++ SwiftUI-MVVM-C/SwiftUI-MVVM-C/Modules/RepoList/RepoList/RepoCell.swift
@@ -11,20 +11,24 @@ struct RepoCell: View {
     let repo: Repo
     
     var body: some View {
-        VStack {
-            Text(repo.name ?? "")
-                .leadingAlignment()
-            
-            if (repo.fork ?? false) {
-                Text("Forked from other repo")
-                    .font(.caption)
+        VStack(spacing: 10) {
+            VStack {
+                Text(repo.name ?? "")
                     .leadingAlignment()
+                
+                if (repo.fork ?? false) {
+                    Text("Forked from other repo")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                        .leadingAlignment()
+                }
             }
             
             if let description = repo.description {
                 Text(description)
                     .font(.caption)
                     .foregroundColor(.secondary)
+                    .lineLimit(2)
                     .leadingAlignment()
             }
         }
