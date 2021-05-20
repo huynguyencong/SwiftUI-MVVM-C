205f6ca4f1a8d30dbfa2c15ad03b3a9492161d35
Huy Nguyen <conghuy2012@gmail.com>
2021-05-20 12:33:42 +0700

    Add view model testing example.

--- SwiftUI-MVVM-C/SwiftUI-MVVM-CTests/SwiftUI_MVVM_CTests.swift
+++ SwiftUI-MVVM-C/SwiftUI-MVVM-CTests/SwiftUI_MVVM_CTests.swift
@@ -17,6 +17,7 @@ class SwiftUI_MVVM_CTests: XCTestCase {
     override func tearDownWithError() throws {
     }
     
+    // An example of network provider testing
     func testGithubNetworkProviderGetRepos() {
         let exp = expectation(description: "Parse repos success")
         var subscriptions = Set<AnyCancellable>()
@@ -35,6 +36,19 @@ class SwiftUI_MVVM_CTests: XCTestCase {
         }.store(in: &subscriptions)
 
         wait(for: [exp], timeout: 0.5)
+    }
+    
+    // An example of view model testing
+    func testForkedRepoTextInRepoDetailsViewModel() {
+        var repo = Repo()
+        
+        var sourceRepo = Repo()
+        sourceRepo.fullName = "sourceOwnerName/SourceRepo"
+        repo.source = Container(value: sourceRepo)
+        
+        let viewModel = RepoDetailsViewModel()
+        viewModel.repo = repo
+        XCTAssertEqual(viewModel.forkText, "Forked from \(sourceRepo.fullName ?? "")")
     }
 
 }
